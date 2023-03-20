9c9e9dbc0f8c8636c9bb1d766d69e3aa74837001
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-03-20 20:42:27 +0200

    add login page

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def landing():
 def signup():
     return render_template('signup.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
