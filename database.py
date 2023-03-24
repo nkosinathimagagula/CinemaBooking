a2b42766281740c5d9950b7702573235dcedcb45
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-03-24 22:08:50 +0200

    add user table in the database

--- database.py
+++ database.py
@@ -6,3 +6,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cineverse_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# TABLES
+class User(db.Model):
+    # COLUMNS
+    user_id = db.Column(db.Integer, nullable=False, primary_key=True, unique=True)
+    firstname = db.Column(db.String(30), nullable=False)
+    lastname = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, firstname, lastname, email, password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
