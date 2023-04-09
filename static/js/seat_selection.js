87766541d4ce64308068a9c77d9d85b83fa7800a
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-04-09 13:26:29 +0200

    add logic and edit page styling

--- static/js/seat_selection.js
+++ static/js/seat_selection.js
@@ -1,18 +1,30 @@
 const container = document.querySelector('.container');
+const num_of_seats_selected = document.getElementById('num_of_seats_selected');
+const seats_selected = document.getElementById('seats_selected');
+const total_price = document.getElementById('total_price');
+
+const ticket_price = document.getElementById('movie_price').getAttribute('data-value');
+
 var seats = []
 
 container.addEventListener('click', (event) => {
     if ( event.target.classList.contains('available_seat') ) {
         event.target.classList.toggle('selected_seat');
-        //
-        console.log(event.target.classList.contains('available_seat'));
 
         if ( seats.includes(event.target.getAttribute('data-value')) ) {
             seats = seats.filter( (element) => element !== event.target.getAttribute('data-value') );
+
+            updateVars()
         } else {
             seats.push(event.target.getAttribute('data-value'));
+
+            updateVars()
         }
-        //
-        console.log(seats);
     }
 });
+
+function updateVars() {
+    num_of_seats_selected.innerHTML = seats.length;
+    total_price.innerHTML = ticket_price * seats.length;
+    seats_selected.innerHTML = seats;
+}
